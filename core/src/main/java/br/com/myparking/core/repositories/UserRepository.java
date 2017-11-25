a04ae9e6fe1df0b3852f51e94e9af3f4a5d52968
LaerteGM <laerteguedes8@gmail.com>
2017-11-25 17:03:38 -0300

    Using Optional user repository (Refactory)

--- core/src/main/java/br/com/myparking/core/repositories/UserRepository.java
+++ core/src/main/java/br/com/myparking/core/repositories/UserRepository.java
@@ -4,10 +4,14 @@ import br.com.myparking.core.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Created by laerteguedes on 26/08/17.
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
 }
